fe2c177cce0628a8095e1b6affc767ea985bdd5c
appshit42 <aap42889@gmail.com>
2026-09-18 22:36:20 +0530

    Fix variable name from 'roundkey' to 'round_key'

--- aes/round_key.v
+++ aes/round_key.v
@@ -1,3 +1,3 @@
-module round_key(input [15:0] state,input [15:0] roundkey,output [15:0] out);
+module round_key(input [15:0] state,input [15:0] round_key,output [15:0] out);
     assign out = state ^ round_key;
 endmodule
